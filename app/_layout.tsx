433d0036915cd7604b0c2a2851d89029b9ebfef5
Ni1esh-Yadav <yadav.nilesh9172@gmail.com>
2025-02-11 01:34:25 +0530

    basic changes

---
+++ app/_layout.tsx
@@ -0,0 +1,10 @@
+import { Stack } from "expo-router";
+
+export default function Layout() {
+  return (
+    <Stack screenOptions={{ headerShown: true }}>
+      <Stack.Screen name="index" options={{ title: "Home" }} />
+      <Stack.Screen name="profile" options={{ title: "About Us" }} />
+    </Stack>
+  );
+}
