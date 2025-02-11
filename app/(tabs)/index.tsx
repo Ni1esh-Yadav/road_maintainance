3a6eac3c170b44bded49fc55d4b7b06a7495c9bd
Sachin Suvarna <sachinsuvarna155@gmail.com>
2025-02-11 13:36:26 +0530

    random commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,27 +1,15 @@
 import { Text, View,StyleSheet } from "react-native";
 
 export default function Index() {
-
-  const styles = StyleSheet.create({
-    container :{
-      flex: 1,
-      justifyContent: "center",
-      alignItems: "center",
-    },
-    text:{
-      fontSize: 20,
-      fontWeight: "bold",
-      justifyContent: "center",
-      alignItems: "center",
-    }
-  })
-
   return (
     <View
-      style={styles.container}
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
     >
-      <Text style = {styles.text}>Edit app/index.tsx to edit this screen.</Text>
+      <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
-  )
-   
+  );
 }
